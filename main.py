bc2533f67bb4493ba4cdd28fb68372edb0a0e76e
Itay Dali <58566230+itay9@users.noreply.github.com>
2022-05-28 13:31:22 +0300

    Update main.py
    fixed path and create txt file

--- main.py
+++ main.py
@@ -14,12 +14,15 @@ def get_data_by_class(bs_soup,cls):
     result = bs_soup.find(class_=cls)
     return result.get_text()
 
-def make_new_dir(newPath):
+def make_new_dir(name,level,lang):
+    # print(newPath)
+    name = str(name)
+    level = str(level)
+    lang = str(lang)
+    currentPath = os.getcwd()
+    newPath = '\\'.join([currentPath, level, name, lang])
     if not os.path.exists(newPath):
         os.makedirs(newPath)
-        name = newPath.split('/')[-2]
-        level = newPath.split('/')[-3]
-        lang = newPath.split('/')[-1]
         print('dir',name,'in level',level,'with',lang,'has been created')
 
 def get_name_of_kata(soup):
@@ -37,27 +40,29 @@ def get_level_of_kata(soup):
     cls = "inner-small-hex is-extra-wide"
     return get_data_by_class(soup,cls).split()[0]
 
-def build_url(name,level,lang):
-    name = str(name)
-    level=str(level)
-    lang=str(lang)
-    currentPath = os.getcwd()
-    url = '\\'.join([currentPath,level,name,lang])
-    print(url)
-
 def get_url():
     print('enter url')
     return input()
 
-def get_soup():
-    URL = get_url()
+def make_link_txt_file(url,path):
+    text_file = open(path+"\\link.txt", "w")
+    text_file.write(url)
+    text_file.close()
+
+
+def get_soup(URL):
     page = requests.get(URL)
     tmp = BeautifulSoup(page.content, "html.parser")
     return tmp
 def make_dir():
-    soup = get_soup()
-    newPath = build_url(get_name_of_kata(soup), get_level_of_kata(soup), get_lang())
-    make_new_dir(newPath)
+    URL = get_url()
+    soup = get_soup(URL)
+    name = get_name_of_kata(soup)
+    level = get_level_of_kata(soup)
+    make_new_dir (name , level, get_lang())
+    currentPath = os.getcwd()
+    path = '\\'.join([currentPath, level, name,])
+    make_link_txt_file(URL,path)
 
 
 make_dir()
